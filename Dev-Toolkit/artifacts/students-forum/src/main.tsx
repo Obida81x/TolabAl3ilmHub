fc708e65369301349feaa28bb7b8a61f89d60535
Obida81x <obidakhaled1@gmail.com>
2026-05-02 09:32:56 +0300

    lets do it

--- Dev-Toolkit/artifacts/students-forum/src/main.tsx
+++ Dev-Toolkit/artifacts/students-forum/src/main.tsx
@@ -2,5 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { supabase } from '@/lib/supabase'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+<div id="root"></div>
 createRoot(document.getElementById("root")!).render(<App />);
